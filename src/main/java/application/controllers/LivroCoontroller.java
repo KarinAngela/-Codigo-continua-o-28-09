859364f50c2f5f20681350c34d4a021d5c70a698
KarinAngela <kahnogueira0407@gmail.com>
2021-10-05 04:18:53 +0000

    atualização nova

--- src/main/java/application/controllers/LivroCoontroller.java
+++ src/main/java/application/controllers/LivroCoontroller.java
@@ -1,10 +1,20 @@
 package application.controllers;
 
-
+//import org.springframework.beans.factory.annotation.Autowi
 import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.RequestMapp
+
 
 @Controller
+//@RequestMapping("/livro")
 public class LivroCoontroller {
+    /* @Autowired
+     @RequestMapping("/list")
+     public String list(Model model) {
+         model.addAtribute("livros");
+         return "list.jsp";
+     }*/
     
 
 
